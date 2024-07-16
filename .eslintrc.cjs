c38292307fb9af5a56543c55d6bf7937859bab50
Juan Gonon <juanfegonon27@gmail.com>
2024-07-16 09:43:16 -0600

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -5,11 +6,22 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:react/jsx-runtime"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', '@typescript-eslint', 'react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types" : "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
